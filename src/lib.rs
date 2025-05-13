baeefafdd30f861aaa675b9c0215995aab484b86
Josh McKinney <joshka@users.noreply.github.com>
2025-05-13 09:50:54 -0700

    chore: fix lints and docs

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@
 //!
 //! Supports:
 //!
-//! - [x] Cohen-Sutherland
+//! - [x] [Cohen-Sutherland](crate::cohen_sutherland)
 //!
 //! TODO
 //!
@@ -30,10 +30,9 @@
 //! use line_clipping::cohen_sutherland::clip_line;
 //! use line_clipping::{LineSegment, Point, Window};
 //!
-//! let line = clip_line(
-//!     LineSegment::new(Point::new(0.0, 0.0), Point::new(10.0, 10.0)),
-//!     Window::new(1.0, 9.0, 1.0, 9.0),
-//! );
+//! let line = LineSegment::new(Point::new(-10.0, -10.0), Point::new(20.0, 20.0));
+//! let window = Window::new(0.0, 10.0, 0.0, 10.0);
+//! let clipped_line = clip_line(line, window);
 //! ```
 //!
 //! # License
@@ -59,46 +58,63 @@ pub mod cohen_sutherland;
 /// A point in 2D space.
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Point {
+    /// The x coordinate of the point.
     pub x: f64,
+
+    /// The y coordinate of the point.
     pub y: f64,
 }
 
 impl Point {
-    pub const ORIGIN: Point = Point { x: 0.0, y: 0.0 };
+    /// A point at the origin (0.0, 0.0).
+    pub const ORIGIN: Self = Self { x: 0.0, y: 0.0 };
 
     /// Creates a new point.
+    #[must_use]
     pub const fn new(x: f64, y: f64) -> Self {
-        Point { x, y }
+        Self { x, y }
     }
 }
 
 /// A line segment in 2D space.
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct LineSegment {
+    /// The first point of the line segment.
     pub p1: Point,
+
+    /// The second point of the line segment.
     pub p2: Point,
 }
 
 impl LineSegment {
     /// Creates a new line segment.
+    #[must_use]
     pub const fn new(p1: Point, p2: Point) -> Self {
-        LineSegment { p1, p2 }
+        Self { p1, p2 }
     }
 }
 
 /// A rectangular region to clip lines against.
 #[derive(Debug, Clone, Copy)]
 pub struct Window {
+    /// The minimum x coordinate of the window.
     pub x_min: f64,
+
+    /// The maximum x coordinate of the window.
     pub x_max: f64,
+
+    /// The minimum y coordinate of the window.
     pub y_min: f64,
+
+    /// The maximum y coordinate of the window.
     pub y_max: f64,
 }
 
 impl Window {
     /// Creates a new window.
+    #[must_use]
     pub const fn new(x_min: f64, x_max: f64, y_min: f64, y_max: f64) -> Self {
-        Window {
+        Self {
             x_min,
             x_max,
             y_min,
